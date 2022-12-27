0d0cd6c5d7b0594e24d599074bc8b0887ed45bec
rira <101010hayakawa@gmail.com>
2022-12-27 13:38:27 +0900

    lオプションを追加
    lオプションでパス指定なしの場合の処理を追加

--- 04.ls/lib/ls.rb
+++ 04.ls/lib/ls.rb
@@ -3,9 +3,30 @@
 
 require 'debug'
 require 'optparse'
+require 'etc'
 
 COLUMNS = 3
 SPACE_FOR_COLUMNS = 2
+TYPE_LIST = {
+  file: '-',
+  directory: 'd',
+  characterSpecial: 'c',
+  blockSpecial: 'b',
+  fifo: 'p',
+  link: 'l',
+  socket: 's'
+}.freeze
+
+MODE_LIST = {
+  7 => 'rwx',
+  6 => 'rw-',
+  5 => 'r-x',
+  4 => 'r--',
+  3 => '-wx',
+  2 => '-w-',
+  1 => '--x',
+  0 => '---'
+}.freeze
 
 def adjust_list_to_display(files)
   rows = (files.size.to_f / COLUMNS).ceil
@@ -45,7 +66,7 @@ def parse_option
   # TODO: オプションの説明追加
   opt.on('-a', '.で始まる要素も表示します')
   opt.on('-r', '逆順でソートして表示します')
-  opt.on('-l', '今後対応予定')
+  opt.on('-l', 'ファイルやディレクトリの詳細なリストを表示します')
   opt.banner = 'Usage: ls [-a][-r][-l]'
   options = {}
   opt.parse!(ARGV, into: options)
@@ -55,6 +76,8 @@ end
 def make_display_list(parse_result)
   result = []
   paths, options = parse_result
+  return make_long_format_list(options, paths) if options[:l]
+
   flag = options[:a] ? File::FNM_DOTMATCH : 0
   if paths == []
     file_list = Dir.glob('*', base: Dir.pwd, flags: flag).sort
@@ -90,6 +113,63 @@ end
 def list_file_paths(paths, need_reverse_order)
   result = paths.select { |path| File::Stat.new(path).file? }.sort
   need_reverse_order ? result.reverse! : result
+end
+
+def make_long_format_list(_options, paths)
+  file_list = Dir.glob('*', base: Dir.pwd).sort
+  total_blocks = 0
+  link_max_char_length = 0
+  user_name_max_char_length = 0
+  group_name_max_char_length = 0
+  file_size_max_char_length = 0
+  file_name_max_char_length = 0
+
+  file_details = file_list.map do |file|
+    stat = File::Stat.new(file)
+    # statの1ブロック単位は512byte
+    # lsコマンドでの1ブロック単位1024byteに合わせるため2で割る
+    total_blocks += stat.blocks / 2
+
+    link_max_char_length = return_larger(stat.nlink.to_s.length, link_max_char_length)
+    user_name_max_char_length = return_larger(Etc.getpwuid(stat.uid).name.to_s.length, user_name_max_char_length)
+    group_name_max_char_length = return_larger(Etc.getgrgid(stat.gid).name.to_s.length, group_name_max_char_length)
+    file_size_max_char_length = return_larger(stat.size.to_s.length, file_size_max_char_length)
+    file_name_max_char_length = return_larger(file.length, file_name_max_char_length)
+
+    [TYPE_LIST[stat.ftype.to_s],
+     get_file_mode(stat),
+     stat.nlink,
+     Etc.getpwuid(stat.uid).name,
+     Etc.getgrgid(stat.gid).name,
+     stat.size,
+     stat.mtime,
+     file]
+  end
+
+  result = +"total #{total_blocks}\n"
+  file_details.each do |file_detail|
+    result << "#{file_detail[0]}#{file_detail[1]} "\
+      "#{file_detail[2].to_s.rjust(link_max_char_length)} "\
+      "#{file_detail[3].rjust(user_name_max_char_length)} "\
+      "#{file_detail[4].rjust(group_name_max_char_length)} "\
+      "#{file_detail[5].to_s.rjust(file_size_max_char_length)} "\
+      "#{file_detail[6].strftime('%b %e %R')} "\
+      "#{file_detail[7]}\n"
+  end
+  result
+end
+
+def return_larger(num1, num2)
+  num1 >= num2 ? num1 : num2
+end
+
+def get_file_mode(stat)
+  result = +''
+  mode = stat.mode.to_s(8)[2..4]
+  mode.each_char do |char|
+    result << MODE_LIST[char.to_i]
+  end
+  result
 end
 
 puts make_display_list(parse_option)
--- 04.ls/test/ls_test.rb
+++ 04.ls/test/ls_test.rb
@@ -184,4 +184,42 @@ class ListTest < Minitest::Test
     #{__dir__}/test_data/03_file #{__dir__}/test_data/04dir`
     assert result.include?('0 failures, 0 errors, 0 skips')
   end
+
+  def test_current_dir_list_display_with_option_l
+    current_dir = Dir.pwd
+    Dir.chdir(test_data_dir)
+    ls_path = "#{__dir__.sub!(%r{/test$}, '')}/lib/ls.rb"
+    output = `ruby #{ls_path} -l`
+    true_str =
+      "total 24\n"\
+      "---rw-r-- 1 rira rira    0 Nov 22 10:03 00_file\n"\
+      "rwxr-xr-x 2 rira rira 4096 Nov 22 10:03 00dir\n"\
+      "---rw-r-- 1 rira rira    0 Nov 22 10:03 01_file\n"\
+      "rwxr-xr-x 2 rira rira 4096 Nov 22 15:15 01dir\n"\
+      "---rw-r-- 1 rira rira    0 Nov 22 10:03 02_file\n"\
+      "rwxr-xr-x 2 rira rira 4096 Nov 22 16:37 02dir\n"\
+      "---rw-r-- 1 rira rira    0 Nov 22 10:03 03_file\n"\
+      "rwxr-xr-x 2 rira rira 4096 Nov 22 14:37 03dir\n"\
+      "---rw-r-- 1 rira rira    0 Nov 22 10:03 04_file\n"\
+      "rwxr-xr-x 3 rira rira 4096 Nov 22 14:30 04dir\n"\
+      "---rw-r-- 1 rira rira    0 Nov 22 10:03 05_file\n"\
+      "---rw-r-- 1 rira rira    0 Nov 22 10:03 06_file\n"\
+      "---rw-r-- 1 rira rira    0 Nov 22 10:03 07_file\n"\
+      "---rw-r-- 1 rira rira    0 Nov 22 10:03 08_file\n"\
+      "---rw-r-- 1 rira rira    0 Nov 22 10:03 09_file\n"\
+      "---rw-r-- 1 rira rira    0 Nov 22 14:54 10_file\n"\
+      "---rw-r-- 1 rira rira    0 Nov 22 10:03 11_file\n"\
+      "---rw-r-- 1 rira rira    0 Nov 22 10:03 12_file\n"\
+      "---rw-r-- 1 rira rira    0 Nov 22 10:03 13_file\n"\
+      "---rw-r-- 1 rira rira    0 Nov 22 10:03 14_file\n"\
+      "---rw-r-- 1 rira rira    0 Nov 22 10:03 15_file\n"\
+      "---rw-r-- 1 rira rira    0 Nov 22 10:03 16_file\n"\
+      "---rw-r-- 1 rira rira    0 Nov 22 10:03 17_file\n"\
+      "---rw-r-- 1 rira rira    0 Nov 22 10:03 18_file\n"\
+      "---rw-r-- 1 rira rira    0 Nov 22 10:03 19_file\n"\
+      "---rw-r-- 1 rira rira    0 Nov 23 09:32 attr_file\n"\
+      "---rw-r-- 1 rira rira  233 Nov 22 11:36 make_test_file.rb\n"
+    assert_equal true_str, output
+    Dir.chdir(current_dir)
+  end
 end
