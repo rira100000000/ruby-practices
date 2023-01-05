c153e2977be86f9422b1ca8f792c0bc2ace1f570
rira <101010hayakawa@gmail.com>
2023-01-05 17:47:49 +0900

    totalを表示する機能を追加
    複数のパスを与えられたとき、totalを表示する機能を追加
    付随してテストを作成

--- 05.wc/lib/wc.rb
+++ 05.wc/lib/wc.rb
@@ -17,18 +17,18 @@ end
 
 def count_lines(path)
   str = File.read(path)
-  str.lines.count.to_s
+  str.lines.count
 end
 
 def count_words(path)
   str = File.read(path)
-  words_number = str.scan(/[\n\t 　]+/).length
-  words_number.to_s
+  number_of_words = str.scan(/[\n\t 　]+/).length
+  number_of_words
 end
 
 def count_bytes(path)
   stat = File::Stat.new(path)
-  stat.size.to_s
+  stat.size
 end
 
 def reset_max_length_hash
@@ -37,47 +37,54 @@ def reset_max_length_hash
     bytes_max_number_of_digits: 0 }
 end
 
-def make_display_line(count_list, max_length_hash)
-  count_list.map do |counts|
+def make_display_line(count_list, max_length_hash, total_hash)
+  result = count_list.map do |counts|
     if counts[:file_not_exists]
       counts[:file_not_exists]
     else
+      display_str = +''
       if counts[:directory]
-        counts[:lines_number] = '0'
-        counts[:words_number] = '0'
-        counts[:bytes_number] = '0'
-        result = +"#{counts[:directory]}\n"
-      else
-        result = +''
+        counts[:number_of_lines] = 0
+        counts[:number_of_words] = 0
+        counts[:number_of_bytes] = 0
+        display_str = +"#{counts[:directory]}\n"
+      elsif counts[:not_file]
+        next display_str = +"#{counts[:not_file]}\n"
       end
-      result << "  #{counts[:lines_number].rjust(max_length_hash[:lines_max_number_of_digits])}"\
-      " #{counts[:words_number].rjust(max_length_hash[:words_max_number_of_digits])}"\
-      " #{counts[:bytes_number].rjust(max_length_hash[:bytes_max_number_of_digits])}"\
+      display_str << "  #{counts[:number_of_lines].to_s.rjust(max_length_hash[:lines_max_number_of_digits])}"\
+      " #{counts[:number_of_words].to_s.rjust(max_length_hash[:words_max_number_of_digits])}"\
+      " #{counts[:number_of_bytes].to_s.rjust(max_length_hash[:bytes_max_number_of_digits])}"\
       " #{counts[:path]}"
-    end
+    end  
   end
+  result << "  #{total_hash[:lines_total]} #{total_hash[:words_total]} #{total_hash[:bytes_total]} total" if result.length > 1 
+  result
 end
 
 def print_all_count(paths)
   max_length_hash = reset_max_length_hash
   count_list = []
+  total_hash = { lines_total: 0, words_total: 0, bytes_total: 0 }
   paths.each_with_index do |path, index|
     count_list[index] = {}
     count_list[index][:path] = path
     if File.directory?(path)
       count_list[index][:directory] = "wc: #{path}: Is a directory"
     elsif File.exist?(path)
-      count_list[index][:lines_number] = count_lines(path)
-      count_list[index][:words_number] = count_words(path)
-      count_list[index][:bytes_number] = count_bytes(path)
-      max_length_hash[:lines_max_number_of_digits] = [count_list[index][:lines_number].length, max_length_hash[:lines_max_number_of_digits]].max
-      max_length_hash[:words_max_number_of_digits] = [count_list[index][:words_number].length, max_length_hash[:words_max_number_of_digits]].max
-      max_length_hash[:bytes_max_number_of_digits] = [count_list[index][:bytes_number].length, max_length_hash[:bytes_max_number_of_digits]].max
+      count_list[index][:number_of_lines] = count_lines(path)
+      count_list[index][:number_of_words] = count_words(path)
+      count_list[index][:number_of_bytes] = count_bytes(path)
+      total_hash[:lines_total] += count_list[index][:number_of_lines]
+      total_hash[:words_total] += count_list[index][:number_of_words]
+      total_hash[:bytes_total] += count_list[index][:number_of_bytes]
     else
       count_list[index][:not_file] = "wc: #{path}: No such file or directory"
     end
+    max_length_hash[:lines_max_number_of_digits] = total_hash[:lines_total].to_s.length
+    max_length_hash[:words_max_number_of_digits] = total_hash[:words_total].to_s.length
+    max_length_hash[:bytes_max_number_of_digits] = total_hash[:bytes_total].to_s.length
   end
-  puts make_display_line(count_list, max_length_hash)
+  puts make_display_line(count_list, max_length_hash, total_hash)
 end
 
 def print_selected_count(paths, options)
--- 05.wc/test/wc_test.rb
+++ 05.wc/test/wc_test.rb
@@ -1,1 +1,34 @@
 # frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative '../lib/wc'
+
+class Word_Count_Test < Minitest::Test
+  attr_reader :test_data_dir, :current_dir, :wc_path
+
+  def setup
+    @test_data_dir = "#{__dir__}/test_data"
+    @current_dir = Dir.pwd
+    Dir.chdir(test_data_dir)
+    @wc_path = "#{__dir__.sub!(%r{/test$}, '')}/lib/wc.rb"
+  end
+
+  def teardown
+    Dir.chdir(current_dir)
+  end
+
+  def test_file_exists
+    output = `ruby #{wc_path} 01_file.txt`
+    expected_str = "  12 18 146 01_file.txt\n"
+    assert_equal output, expected_str
+  end
+
+  def test_multiple_files_exist
+    output = `ruby #{wc_path} 01_file.txt 03_file.txt`
+    expected_str =
+    "   12  18 146 01_file.txt\n"\
+    "   99  99 198 03_file.txt\n"\
+    "  111 117 344 total\n"
+    assert_equal output, expected_str
+  end
+end
