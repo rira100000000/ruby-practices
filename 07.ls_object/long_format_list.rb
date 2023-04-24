a22f1ce33a71c595748d1f55e126189715ec8559
rira <101010hayakawa@gmail.com>
2023-04-24 14:14:15 +0900

    empty?メソッドを使用するように修正
    配列の空判定でempty?メソッドを使用するように修正しました。

--- 07.ls_object/long_format_list.rb
+++ 07.ls_object/long_format_list.rb
@@ -7,7 +7,7 @@ class LongFormatList < List
   def self.make_long_format_list(paths, options)
     max_length_hash = reset_max_length_hash
     flag = options[:a] ? File::FNM_DOTMATCH : 0
-    if paths == []
+    if paths.empty?
       file_list = Dir.glob('*', base: Dir.pwd, flags: flag).sort
       file_list.reverse! if options[:r]
       result = list_long_format_list_to_display(file_list.map { |file| FileDetail.fetch_file_details(file, max_length_hash) }, max_length_hash)
